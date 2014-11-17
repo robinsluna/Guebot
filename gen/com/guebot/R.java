ed9c8bfad3b18b0e525fa4fbbb8418a045764825
robins de la asuncion <ing.robins@gmail.com>
2014-11-16 22:55:43 -0500

    cambios

--- gen/com/guebot/R.java
+++ gen/com/guebot/R.java
@@ -11,25 +11,31 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static int ic_abajo=0x7f020000;
-        public static int ic_agarro=0x7f020001;
-        public static int ic_arriba=0x7f020002;
-        public static int ic_launcher=0x7f020003;
+        public static final int ic_abajo=0x7f020000;
+        public static final int ic_agarro=0x7f020001;
+        public static final int ic_arriba=0x7f020002;
+        public static final int ic_calibrar=0x7f020003;
+        public static final int ic_launcher=0x7f020004;
+        public static final int ic_soltar=0x7f020005;
     }
     public static final class id {
-        public static int button_abajo=0x7f060003;
-        public static int button_agarrar=0x7f060002;
-        public static int button_arriba=0x7f060001;
-        public static int imageView=0x7f060000;
+        public static final int button_abajo=0x7f060005;
+        public static final int button_abrir=0x7f060003;
+        public static final int button_arriba=0x7f060002;
+        public static final int button_calibrar=0x7f060001;
+        public static final int button_cerrar=0x7f060004;
+        public static final int imageView=0x7f060000;
     }
     public static final class layout {
-        public static int activity_main=0x7f030000;
+        public static final int activity_main=0x7f030000;
     }
     public static final class string {
-        public static int app_name=0x7f040000;
-        public static int btn_abajo=0x7f040002;
-        public static int btn_agarrar=0x7f040003;
-        public static int btn_arriba=0x7f040001;
+        public static final int app_name=0x7f040000;
+        public static final int btn_abajo=0x7f040002;
+        public static final int btn_abrir=0x7f040003;
+        public static final int btn_arriba=0x7f040001;
+        public static final int btn_calibrar=0x7f040005;
+        public static final int btn_cerrar=0x7f040004;
     }
     public static final class style {
         /** 
@@ -53,10 +59,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static int AppBaseTheme=0x7f050000;
+        public static final int AppBaseTheme=0x7f050000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static int AppTheme=0x7f050001;
+        public static final int AppTheme=0x7f050001;
     }
 }
